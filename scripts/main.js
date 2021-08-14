cc81635fb745ef0110b5e2c220f5820fec45d270
chupper100 <phamminhdat223@gmail.com>
2021-08-14 09:42:31 +0700

    test

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,1 @@
-for (let i = 1; i <= 5; i++) {
-    setTimeout(() => window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-
-}
+window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
