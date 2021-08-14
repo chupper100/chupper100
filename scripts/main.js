63de92d09da668b315734fafb5a2cc5675d573a6
chupper100 <phamminhdat223@gmail.com>
2021-08-14 09:38:12 +0700

    test

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,4 @@
-window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+for (let i = 1; i <= 4; i++) {
+    setTimeout(() => window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+
+}
